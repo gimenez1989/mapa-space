e5c4fc8033f4a5e8b9d1436c947baccd11942516
Carlos Gimenez <gimenez.22axl@gmail.com>
2021-12-07 21:59:57 -0300

    boton para hacer invisile a capa de la luna

--- src/Mapa-Space/index.js
+++ src/Mapa-Space/index.js
@@ -1,45 +1,89 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Map } from "maplibre-gl"
-import { Container } from '@material-ui/core'
+import { Container, Button, Box } from '@material-ui/core'
 import useStyles from './styles'
 
-const MapaSpace = ({ map }) => {
+const MapaSpace = () => {
+    const [map, setMap] = useState(null)
+    const [visibility, setVisibility] = useState(false)
     const classes = useStyles()
 
-    useEffect(() => {
-        new Map({
-            container: 'map',
-            style: {
-                version: 8,
-                sources: {
-                    Moon: {
-                        type: "raster",
-                        scheme: "tms",
-                        tiles: [
-                            "https://cartocdn-gusc.global.ssl.fastly.net/opmbuilder/api/v1/map/named/opm-moon-basemap-v0-1/all/{z}/{x}/{y}.png",
-                        ],
-                        tileSize: 256,
-                    }
-                },
-                layers: [
-                    {
-                        id: "layermoon",
-                        type: "raster",
-                        source: "Moon",
-                        layout: {
-                            visibility: "visible"
-                        }
-                    }
-                ]
-            },
-            zoom: 4,
-            center: [0, 0]
-        })
+    const Change = () => {
+        if (visibility === false) {
+            map.setLayoutProperty("layermars", "visibility", "visible")
+            map.setLayoutProperty("layermoon", "visibility", "none")
+        }
+        setVisibility(!visibility)
+    }
 
-    }, [map])
+    useEffect(() => {
+        setMap(
+            new Map({
+                container: 'map',
+                style: {
+                    version: 8,
+                    sources: {
+                        Moon: {
+                            type: "raster",
+                            scheme: "tms",
+                            tiles: [
+                                "https://cartocdn-gusc.global.ssl.fastly.net/opmbuilder/api/v1/map/named/opm-moon-basemap-v0-1/all/{z}/{x}/{y}.png",
+                            ],
+                            tileSize: 256,
+                        },
+                        Mars: {
+                            type: "raster",
+                            scheme: "tms",
+                            tiles: [
+                                "https://cartocdn-gusc.global.ssl.fastly.net/opmbuilder/api/v1/map/named/opm-mars-basemap-v0-2/all/{z}/{x}/{y}.png",
+                            ],
+                            tileSize: 256,
+                        }
+                    },
+                    layers: [
+                        {
+                            id: "layermoon",
+                            type: "raster",
+                            source: "Moon",
+                            layout: {
+                                visibility: "visible"
+                            }
+                        },
+                        {
+                            id: "layermars",
+                            type: "raster",
+                            source: "Mars",
+                            layout: {
+                                visibility: "none"
+                            }
+                        }
+                    ]
+                },
+                zoom: 5,
+                center: [0, 0]
+            })
+        )
+    }, [])
 
     return (
-        <Container className={classes.mapContainer} id="map" />
+        <Container className={classes.mapContainer} id="map">
+           { /* <Box>
+                <Button className={classes.Moon}
+                    onClick={() => {
+                        Change()
+                    }}>
+                    <p>Moon</p>
+                </Button>
+            </Box>*/}
+            <Box>
+                <Button className={classes.Mars}
+                    onClick={() => {
+                        Change();
+                    }}>
+                    <p>Mars</p>
+                </Button>
+                </Box>
+        </Container >
     )
 }
 
--- src/Mapa-Space/styles.js
+++ src/Mapa-Space/styles.js
@@ -9,5 +9,15 @@ export default makeStyles((theme) => ({
         maxWidth: '100%',
         padding: 0,
         lineHeight: 0
+    },
+    Moon: {
+        background: 'blue',
+        color: 'white',
+        position: 'relative; z-index:1'
+    },
+    Mars: {
+        background: 'red',
+        color: 'white',
+        position: 'relative; z-index:1'
     }
 }))
